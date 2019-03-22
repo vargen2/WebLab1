33e184b1d9372a8aec21586bf709602b8e988781
vargen2 <hjaarn2@gmail.com>
2019-03-23 00:08:49 +0100

    change to buttons

--- main.js
+++ main.js
@@ -33,18 +33,6 @@ function setQuestion(question) {
     answers.push(question.correct_answer)
     shuffle(answers)
 
-
-
-    document.getElementById('answer1').checked = false
-    document.getElementById('answer2').checked = false
-    document.getElementById('answer3').checked = false
-    document.getElementById('answer4').checked = false
-
-    document.getElementById('label-answer1').innerText = answers[0]
-    document.getElementById('label-answer2').innerText = answers[1]
-    document.getElementById('label-answer3').innerText = answers[2]
-    document.getElementById('label-answer4').innerText = answers[3]
-
     document.getElementById('button1').innerText = answers[0]
     document.getElementById('button2').innerText = answers[1]
     document.getElementById('button3').innerText = answers[2]
@@ -52,7 +40,6 @@ function setQuestion(question) {
 }
 
 function answerFunc(nr) {
-    console.log(nr)
     var answer = answers[nr]
     var correct_answer = currentQuestion.correct_answer
     if (answer === correct_answer) {
@@ -70,37 +57,8 @@ function answerFunc(nr) {
 
 
 window.addEventListener('load', () => {
-
-    
-   
-
-    // document.getElementById('form').addEventListener('submit', function (e) {
-    //     e.preventDefault();
-    //     var formData = new FormData(e.target)
-    //     var answer = answers[formData.get('answer')]
-    //     var correct_answer = currentQuestion.correct_answer
-    //     if (answer === correct_answer) {
-    //         correct++
-    //         document.getElementById('response').innerHTML = 'Correct!'
-    //     }else {
-    //         document.getElementById('response').innerHTML = 'Wrong!'
-    //     }
-    //     current++
-    //     if (current < 10)
-    //         setQuestion(questions[current])
-    //     else
-    //         endQuiz()
-    // }, false);
-
-    // document.getElementById('submit').onclick = function () {
-    //     document.getElementById('form').submit();
-    // }
-
-
     loader().then(r => {
         questions = r
         setQuestion(questions[current])
     })
-
-
 });
