01c5c7b985e60f82c66e0a3af9f6ee826990c609
vargen2 <hjaarn2@gmail.com>
2019-03-22 23:51:12 +0100

    before change to buttons

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+var questions
+var currentQuestion
+var answers
+var correct = 0
+var current = 0
 
-
-/**
- * Shuffles array in place. ES6 version
- * @param {Array} a items An array containing the items.
- */
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -12,72 +12,89 @@ function shuffle(a) {
 }
 
 async function loader() {
-
-
-  
-
-     var url = 'https://opentdb.com/api.php?amount=10&type=multiple'
-     const res = await fetch(url)
-     const json = await res.json()
-   // const json = mockk
+    var url = 'https://opentdb.com/api.php?amount=10&type=multiple'
+    const res = await fetch(url)
+    const json = await res.json()
     return json.results
-
 }
 
+function endQuiz() {
+    document.getElementById('progress').innerHTML = 'Quiz completed. ' + correct + 'p/10p'
+    document.getElementById('question').innerHTML = ''
+    document.getElementById('choices').innerHTML = ''
+}
+
+function setQuestion(question) {
+    currentQuestion = question
+    document.getElementById('progressBar').value=current
+    document.getElementById('question').innerHTML = question.question
+    document.getElementById('progress').innerHTML = 'Question: ' + current + ' / 10'
+    answers = question.incorrect_answers
+    answers.push(question.correct_answer)
+    shuffle(answers)
+
+
+
+    document.getElementById('answer1').checked = false
+    document.getElementById('answer2').checked = false
+    document.getElementById('answer3').checked = false
+    document.getElementById('answer4').checked = false
+
+    document.getElementById('label-answer1').innerText = answers[0]
+    document.getElementById('label-answer2').innerText = answers[1]
+    document.getElementById('label-answer3').innerText = answers[2]
+    document.getElementById('label-answer4').innerText = answers[3]
+
+    document.getElementById('button1').innerText = answers[0]
+    document.getElementById('button2').innerText = answers[1]
+    document.getElementById('button3').innerText = answers[2]
+    document.getElementById('button4').innerText = answers[3]
+}
+
+function answerFunc(nr) {
+    console.log(nr)
+    var answer = answers[nr]
+    var correct_answer = currentQuestion.correct_answer
+    if (answer === correct_answer) {
+        correct++
+        document.getElementById('response').innerHTML = 'Correct!'
+    }else {
+        document.getElementById('response').innerHTML = 'Wrong!'
+    }
+    current++
+    if (current < 10)
+        setQuestion(questions[current])
+    else
+        endQuiz()
+}
 
 
 window.addEventListener('load', () => {
 
-    function endQuiz() {
-        document.getElementById('progress').innerHTML = 'Quiz completed. ' + correct +'p/10p'
-        document.getElementById('question').innerHTML = ''
-        document.getElementById('choises').innerHTML = ''
-    }
+    
+   
 
-    function setQuestion(question) {
-        currentQuestion = question
-        document.getElementById('question').innerHTML = question.question
-        document.getElementById('progress').innerHTML = 'question: ' + current + '/10, correct: ' + correct
-        answers = question.incorrect_answers
-        answers.push(question.correct_answer)
-        shuffle(answers)
+    // document.getElementById('form').addEventListener('submit', function (e) {
+    //     e.preventDefault();
+    //     var formData = new FormData(e.target)
+    //     var answer = answers[formData.get('answer')]
+    //     var correct_answer = currentQuestion.correct_answer
+    //     if (answer === correct_answer) {
+    //         correct++
+    //         document.getElementById('response').innerHTML = 'Correct!'
+    //     }else {
+    //         document.getElementById('response').innerHTML = 'Wrong!'
+    //     }
+    //     current++
+    //     if (current < 10)
+    //         setQuestion(questions[current])
+    //     else
+    //         endQuiz()
+    // }, false);
 
-
-
-        document.getElementById('answer1').checked = false
-        document.getElementById('answer2').checked = false
-        document.getElementById('answer3').checked = false
-        document.getElementById('answer4').checked = false
-
-        document.getElementById('label-answer1').innerText = answers[0]
-        document.getElementById('label-answer2').innerText = answers[1]
-        document.getElementById('label-answer3').innerText = answers[2]
-        document.getElementById('label-answer4').innerText = answers[3]
-    }
-
-    var questions
-    var currentQuestion
-    var answers
-    var correct = 0
-    var current = 0
-
-    document.getElementById('answerform').addEventListener('submit', function (e) {
-        e.preventDefault();
-        var formData = new FormData(e.target)
-        var answer = answers[formData.get('answer')]
-        var correct_answer = currentQuestion.correct_answer
-        if (answer === correct_answer)
-            correct++
-        current++
-        if (current < 10)
-            setQuestion(questions[current])
-        else
-            endQuiz()
-    }, false);
-
-    document.getElementById('submit').onclick = function () {
-        document.getElementById('answerform').submit();
-    }
+    // document.getElementById('submit').onclick = function () {
+    //     document.getElementById('form').submit();
+    // }
 
 
     loader().then(r => {
