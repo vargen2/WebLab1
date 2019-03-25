16afe2e97391433a59fc3c720b4c9e887967e4ca
vargen2 <hjaarn2@gmail.com>
2019-03-25 22:49:58 +0100

    eslint

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-async function loader() {
-    return fetch('https://opentdb.com/api_category.php')
-        .then(res => res.json())
-        .then(json => json.trivia_categories)
+async function loader () {
+  return fetch('https://opentdb.com/api_category.php')
+    .then(res => res.json())
+    .then(json => json.trivia_categories)
 }
 
-async function sort(categories) {
-    categories.sort((a, b) => {
-        var x = a.name.toLowerCase();
-        var y = b.name.toLowerCase();
-        if (x < y) { return -1; }
-        if (x > y) { return 1; }
-        return 0;
-    })
-    return categories
+async function sort (categories) {
+  categories.sort((a, b) => {
+    var x = a.name.toLowerCase()
+    var y = b.name.toLowerCase()
+    if (x < y) { return -1 }
+    if (x > y) { return 1 }
+    return 0
+  })
+  return categories
 }
 
-async function populateCategorySelector(categories) {
-    const categorySelect = document.getElementById('category')
-    for (const category of categories) {
-        var option = document.createElement("option")
-        option.text = category.name
-        option.value = category.id
-        categorySelect.add(option)
-    }
+async function populateCategorySelector (categories) {
+  const categorySelect = document.getElementById('category')
+  for (const category of categories) {
+    var option = document.createElement('option')
+    option.text = category.name
+    option.value = category.id
+    categorySelect.add(option)
+  }
 }
 
 window.addEventListener('load', () => {
-    loader().then(sort).then(populateCategorySelector)
+  loader().then(sort).then(populateCategorySelector)
 })
--- main.js
+++ main.js
@@ -4,86 +4,81 @@ var answers
 var correct = 0
 var current = 0
 
-async function createApiUrl(url) {
-    let number = url.searchParams.get('n')
-    number = Math.max(1, Math.min((number) ? number : 10, 50))
+async function createApiUrl (url) {
+  let number = url.searchParams.get('n')
+  number = Math.max(1, Math.min((number) || 10, 50))
 
-    let category = url.searchParams.get('c')
+  let category = url.searchParams.get('c')
 
-    let difficulty = url.searchParams.get('d')
-    difficulty = (difficulty) ? difficulty : ''
+  let difficulty = url.searchParams.get('d')
+  difficulty = (difficulty) || ''
 
-    return 'https://opentdb.com/api.php?amount=' + number + '&type=multiple&category=' + category + '&difficulty=' + difficulty
+  return 'https://opentdb.com/api.php?amount=' + number + '&type=multiple&category=' + category + '&difficulty=' + difficulty
 }
 
-async function loader() {
-    const apiUrl = await createApiUrl(new URL(window.location.href))
-    const res = await fetch(apiUrl)
-    const json = await res.json()
-    return json.results
+async function loader () {
+  const apiUrl = await createApiUrl(new URL(window.location.href))
+  const res = await fetch(apiUrl)
+  const json = await res.json()
+  return json.results
 }
 
-function endQuiz() {
-    document.getElementById('progressBar').value = questions.length
-    document.getElementById('progress').innerHTML = 'Quiz completed'
-    document.getElementById('question').innerHTML = correct + ' correct answers out of ' + questions.length
-    document.querySelectorAll('.formElement').forEach(b => b.style.display = 'none')
+function endQuiz () {
+  document.getElementById('progressBar').value = questions.length
+  document.getElementById('progress').innerHTML = 'Quiz completed'
+  document.getElementById('question').innerHTML = correct + ' correct answers out of ' + questions.length
+  document.querySelectorAll('.formElement').forEach(b => { b.style.display = 'none' })
 }
 
-function setQuestion(question) {
-    currentQuestion = question
-    document.getElementById('progressBar').value = current
-    document.getElementById('progressBar').max = questions.length
-    document.getElementById('question').innerHTML = currentQuestion.question
-    document.getElementById('progress').innerHTML = 'Question: ' + current + ' / ' + questions.length
+function setQuestion (question) {
+  currentQuestion = question
+  document.getElementById('progressBar').value = current
+  document.getElementById('progressBar').max = questions.length
+  document.getElementById('question').innerHTML = currentQuestion.question
+  document.getElementById('progress').innerHTML = 'Question: ' + current + ' / ' + questions.length
 
-    answers = currentQuestion.incorrect_answers.slice()
-    answers.splice(Math.floor(Math.random() * 4), 0, currentQuestion.correct_answer)
+  answers = currentQuestion.incorrect_answers.slice()
+  answers.splice(Math.floor(Math.random() * 4), 0, currentQuestion.correct_answer)
 
-    document.getElementById('button1').innerText = answers[0]
-    document.getElementById('button2').innerText = answers[1]
-    document.getElementById('button3').innerText = answers[2]
-    document.getElementById('button4').innerText = answers[3]
+  document.getElementById('button1').innerText = answers[0]
+  document.getElementById('button2').innerText = answers[1]
+  document.getElementById('button3').innerText = answers[2]
+  document.getElementById('button4').innerText = answers[3]
 }
 
-function answerFunc(nr) {
-    if (!answers)
-        return
-    var answer = answers[nr]
-    var correct_answer = currentQuestion.correct_answer
-    if (answer === correct_answer) {
-        correct++
-        document.getElementById('response').innerHTML = 'Correct!'
-        document.getElementById('response').style.color = 'rgb(113,233,113)'
-    } else {
-        document.getElementById('response').innerHTML = 'Wrong!'
-        document.getElementById('response').style.color = 'rgb(233,113,113)'
-    }
-    current++
-    if (current < questions.length)
-        setQuestion(questions[current])
-    else
-        endQuiz()
+function answerFunc (nr) {
+  if (!answers) { return }
+
+  if (answers[nr] === currentQuestion.correct_answer) {
+    correct++
+    document.getElementById('response').innerHTML = 'Correct!'
+    document.getElementById('response').style.color = 'rgb(113,233,113)'
+  } else {
+    document.getElementById('response').innerHTML = 'Wrong!'
+    document.getElementById('response').style.color = 'rgb(233,113,113)'
+  }
+
+  current++
+  if (current < questions.length) { setQuestion(questions[current]) } else { endQuiz() }
 }
 
-function restartSame() {
-    document.querySelectorAll('.formElement').forEach(b => b.style.display = 'inline-block')
-    current = 0
-    correct = 0
-    setQuestion(questions[current])
+function restartSame () {
+  document.querySelectorAll('.formElement').forEach(b => { b.style.display = 'inline-block' })
+  current = 0
+  correct = 0
+  setQuestion(questions[current])
 }
 
 window.addEventListener('load', () => {
-    var loading = setInterval(() => {
-        document.getElementById('question').innerHTML += '.'
+  var loading = setInterval(() => {
+    document.getElementById('question').innerHTML += '.'
+  }, 100)
 
-    }, 100);
-
-    loader().then(r => {
-        clearInterval(loading)
-        questions = r
-        setQuestion(questions[current])
-    }).catch(err=>{
-        document.getElementById('question').innerHTML='Error ' + err.message
-    })
+  loader().then(r => {
+    clearInterval(loading)
+    questions = r
+    setQuestion(questions[current])
+  }).catch(err => {
+    document.getElementById('question').innerHTML = 'Error ' + err.message
+  })
 })
